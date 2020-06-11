0f4ab16f1a5a07bb36a19c644a9533c0016e22b2
Zafer <0970135@hr.nl>
2020-06-11 17:09:27 +0200

    User option added

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class SuperAdmin(User):
         Db.create_user(username, password, "system admin")
         print("System admin has been created.")
         
+superuser = SuperAdmin("super", "super")
         
 class SysAdmin(User):
     def __init__(self, username, password):
@@ -33,14 +34,30 @@ class Advisor(User):
         super().__init__(username, password)
         self.accessLevel = "advisor"
 
+def Option():
+    option = int(input("""  
+            Welcome to the application.\n 
+            Please select an option by entering a number:\n
+            [1] Login\n
+            [2] Password reset\n
+            Your choice: """
+    ))
+    
+    if option == 1:
+        Login()
 
-def Login(username, password):
-    pass
+def Login():
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ")
+    if username == superuser.username and password == superuser.password:
+        print("Logged in as super user")
+    else:
+        print("Login credentials are incorrect. Please try again..")
 
 
 def main():
     Db.main()
-    superuser = SuperAdmin("super", "super")
-    superuser.createSysAdmin("sysadmin", "sysadmin")
+    Option()
+    
 
 main()
