e39a976a58d92853d57980c716714f1088ad7e18
Zafer <0970135@hr.nl>
2020-06-11 16:23:41 +0200

    User creation has been added

--- Db.py
+++ Db.py
@@ -16,7 +16,9 @@ def create_connection(db_file):
 
     return conn
 
-def create_table(conn, create_table_sql):
+conn = create_connection(r"sqlite.db")
+
+def create_table(create_table_sql):
     """ create a table from the create_table_sql statement
     :param conn: Connection object
     :param create_table_sql: a CREATE TABLE statement
@@ -28,7 +30,7 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def create_user_table(conn):
+def create_user_table():
     sql = """ CREATE TABLE IF NOT EXISTS users (
                         user_id INTEGER PRIMARY KEY,
                         username TEXT NOT NULL,
@@ -36,9 +38,9 @@ def create_user_table(conn):
                         accesslevel TEXT NOT NULL
                     );
                     """
-    create_table(conn, sql)
+    create_table(sql)
 
-def create_client_table(conn):
+def create_client_table():
     sql = """ 
             CREATE TABLE IF NOT EXISTS clients (
                 client_id INTEGER PRIMARY KEY,
@@ -50,23 +52,18 @@ def create_client_table(conn):
                 phonenumber TEXT NOT NULL
             );
             """
-    create_table(conn, sql)
+    create_table(sql)
 
-def create_user(conn, username, password, accesslevel):
+def create_user(username, password, accesslevel):
     cursor = conn.cursor()
     sql = """   INSERT INTO users (username, password, accesslevel)
                 VALUES ("%s", "%s", "%s") """ % (username, password, accesslevel)
     cursor.execute(sql)
     conn.commit()
-    print("Value inserted")
     cursor.close()
 
 
-
 def main():
-    conn = create_connection(r"sqlite.db")
     if conn is not None:
-        create_user_table(conn)
-        create_client_table(conn)
-
-
+        create_user_table()
+        create_client_table()
--- main.py
+++ main.py
@@ -11,12 +11,21 @@ class SuperAdmin(User):
     def __init__(self, username, password):
         super().__init__(username, password)
         self.accessLevel = "super admin"
+
+    def createSysAdmin(self, username, password):
+        Db.create_user(username, password, "system admin")
+        print("System admin has been created.")
         
         
 class SysAdmin(User):
     def __init__(self, username, password):
         super().__init__(username, password)
         self.accessLevel = "system admin"
+
+    def createAdvisor(self, username, password):
+        Db.create_user(username, password, "advisor")
+        print("Advisor has been created.")
+        
 
 
 class Advisor(User):
@@ -31,8 +40,7 @@ def Login(username, password):
 
 def main():
     Db.main()
-    
-    
-
+    superuser = SuperAdmin("super", "super")
+    superuser.createSysAdmin("sysadmin", "sysadmin")
 
 main()
