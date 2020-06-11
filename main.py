ca83aff54d018fef10ba283ca7760e73327e7c19
Zafer <0970135@hr.nl>
2020-06-11 13:43:12 +0200

    Base user classes gemaakt

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import abc
 
 class User(abc.ABC):
-    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        
+class SuperAdmin(User):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.accessLevel = "super admin"
+        
+        
+class SysAdmin(User):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.accessLevel = "system admin"
+
+
+class Advisor(User):
+    def __init__(self, username, password):
+        super().__init__(username, password)
+        self.accessLevel = "advisor"
+
 
 def main():
-    print("Done")
+    superadmin = SuperAdmin("User", "Pass")
+    sysadmin = SysAdmin("User", "Pass")
+    advisor = Advisor("User", "Pass")
+    print(superadmin.accessLevel)
+    print(sysadmin.accessLevel)
+    print(advisor.accessLevel)
 
 main()
