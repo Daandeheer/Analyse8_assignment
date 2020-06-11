abdbaaf0257943d7bb86c6d46a934962032fa680
Daan <Daaandeheer@gmail.com>
2020-06-11 20:40:11 +0200

    added all validation field for add client

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import abc
 import re
 
+# extra validation steps:
+# look at capital letters, fails at the moment
+# It is possible to add to spaces next to eachother
+# city needs to be written exactly like in the list
+
+
 class User(abc.ABC):
     def __init__(self, username, password):
         self.username = username
@@ -21,49 +27,74 @@ class SysAdmin(User):
         fullName = input(clientInput)
         output = re.search("^[a-z, \s]{5,31}$", fullName)
         if output == None:
-            print("A name requires at least 5 and at most 30 characters")
+            print("Invalid input: A name requires at least 5 and at most 30 characters, please try again:")
             return self.NameValidation(clientInput)
         else:
             return fullName
         
 
-    def StreetNameValidation(self, clientInput):
-        streetName = input(clientInput)
-        output = re.search("^[a-z, \s]{5,31}$", streetName)
+    def StreetValidation(self, clientInput):
+        street = input(clientInput)
+        output = re.search("^[a-z, \s]{5,31}\s[1-9]([0-9]{0,3})[a-z]?$", street)
         if output == None:
-            print("A name requires at least 5 and at most 30 characters")
-            return self.StreetNameValidation(clientInput)
+            print("Invalid input: Street requires at least 5 and at most 30 characters. Also don't forget to include your house number, please try again:")
+            return self.StreetValidation(clientInput)
         else:
-            return streetName
-        
-    
-    def HouseNumberValidation(self, clientInput):
-        houseNumber = input(clientInput)
-        output = re.search("^[1-9]([0-9]{0,3})[a-z]?$", houseNumber)
+            return street
+
+    def ZipCodeValidation(self, clientInput):
+        zipCode = input(clientInput)
+        output = re.search("^[1-9][0-9]{3}[A-Z]{2}$", zipCode)
         if output == None:
-            print("House Number can only have a maximum of 4 digits and optionally 1 letter")
-            return self.HouseNumberValidation(clientInput)
+            print("Invalid input: Use capital letters and remove space, please try again:")
+            return self.ZipCodeValidation(clientInput)
         else:
-            return houseNumber
-        
-        
+            return zipCode
 
-
-    def AddClient(self):
+    def CityValidation(self, clientInput):
         cityList = ["Amsterdam", "Rotterdam" ,"Maastricht", "Eindhoven", "Den Haag", "Utrecht" ,"Leiden", "Arnhem", "Zwolle","Delft"]
+        print(cityList)
+        city = input(clientInput)
+        for x in cityList:
+            if re.search(x, city) :
+                return city
+            else:
+                print("Invalid input: City don't exist, please choose a city from the list:")
+                return self.CityValidation(clientInput)
+    
+    # Add regex for email
+    def EmailValidation(self, clientInput):
+        emailAddress = input(clientInput)
+        output = re.search("",emailAddress)
+        if output == None:
+            print("Invalid input: Use a real email, please try again:")
+            return self.EmailValidation(clientInput)
+        else:
+            return emailAddress
+
+    def PhoneNumberValidation(self,clientInput):
+        phoneNumber = input(clientInput)
+        output = re.search("^[+][3][1][6][0-9]{8}$",phoneNumber)
+        if output == None:
+            print("Invalid input: Start with +316, please try again:")
+            return self.PhoneNumberValidation(clientInput)
+        else:
+            return phoneNumber
+
+                
+    def AddClient(self):
         if self.accessLevel == "system admin":
             print('Status: System administrator can add a new Client')
             print('Add the following information to register as a client')
             
+
             
             # fullName = self.NameValidation("Full name: ")
-            # streetName = self.StreetNameValidation("Street: ")
-            houseNumber = self.HouseNumberValidation("Housenumber: ")
-
-            # zipCode = input("Zip code: ")
-            # city = input("Choose one of the Citys " + str(cityList) + ": ")
-            # emailAddress = input("Email Address: ")
-            # phoneNumber = input("Phone Number: ")
+            # street = self.StreetValidation("Street: ") 
+            # zipCode = self.ZipCodeValidation("Zip code: ")
+            # city = self.CityValidation("Choose one of the Citys: ")
+            # emailAddress = self.EmailValidation("Email Address: ")
+            # phoneNumber = self.PhoneNumberValidation("Phone Number: ")
             
 
         elif self.accessLevel == "super admin":
