0af14c7567215d96e571c37f9fa43085f90c1ac2
Daan <Daaandeheer@gmail.com>
2020-06-11 17:50:40 +0200

    changed way of validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import abc
+import re
 
 class User(abc.ABC):
     def __init__(self, username, password):
@@ -17,21 +18,35 @@ class SysAdmin(User):
         self.accessLevel = "system admin"
 
     def NameValidation(self, clientInput):
-        while True:
-            try:
-                name = input(clientInput)
-            except ValueError:
-                print("")
-                continue
-            if len(name) < 5 or len(name) > 30:
-                print("A name requires at least 5 and at most 30 characters")
-                continue
-            elif not all(x.isalpha() or x.isspace() for x in name):
-                print("Name can only contain letters")
-                continue
-            else:
-                break
-        return name
+        fullName = input(clientInput)
+        output = re.search("^[a-z, \s]{5,31}$", fullName)
+        if output == None:
+            print("A name requires at least 5 and at most 30 characters")
+            return self.NameValidation(clientInput)
+        else:
+            return fullName
+        
+
+    def StreetNameValidation(self, clientInput):
+        streetName = input(clientInput)
+        output = re.search("^[a-z, \s]{5,31}$", streetName)
+        if output == None:
+            print("A name requires at least 5 and at most 30 characters")
+            return self.StreetNameValidation(clientInput)
+        else:
+            return streetName
+        
+    
+    def HouseNumberValidation(self, clientInput):
+        houseNumber = input(clientInput)
+        output = re.search("^[1-9]([0-9]{0,3})[a-z]?$", houseNumber)
+        if output == None:
+            print("House Number can only have a maximum of 4 digits and optionally 1 letter")
+            return self.HouseNumberValidation(clientInput)
+        else:
+            return houseNumber
+        
+        
 
 
     def AddClient(self):
@@ -41,9 +56,10 @@ class SysAdmin(User):
             print('Add the following information to register as a client')
             
             
-            fullName = self.NameValidation("Full name: ")
-            streetName = input("Street: ")
-            HouseNumber = input("Housenumber: ")
+            # fullName = self.NameValidation("Full name: ")
+            # streetName = self.StreetNameValidation("Street: ")
+            houseNumber = self.HouseNumberValidation("Housenumber: ")
+
             # zipCode = input("Zip code: ")
             # city = input("Choose one of the Citys " + str(cityList) + ": ")
             # emailAddress = input("Email Address: ")
